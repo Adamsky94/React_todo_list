6736c40e99cdc2cfb0d24e73ac35e58a1bb4ae33
Adam Kiss <a.kiss1994@gmail.com>
2021-05-11 09:41:08 +0100

    Enabled adding items to form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import TodoList from "./TodoList"
+import TodoList from "./TodoList";
 import './App.css';
 
 class App extends Component {
   render() {
     return(
       <div>
+        <h1>Todo List</h1>
         <TodoList/>
       </div>
     );
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 
 class TodoList extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={todos:[]}
+        this.state = {
+            todos: [],
+        };
+        this.create = this.create.bind(this);
+    }
+    create(newTodo) {
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        });
     }
     render() {
-        return(
+        const todos = this.state.todos.map((todo) => {
+            return <Todo task={todo.task} />;
+        });
+        return (
             <div>
-                <h1>Todo List!</h1>
-                <ul>
-                    <li>Todo1</li>
-                    <li>Todo2</li>
-                </ul>
+                <NewTodoForm createTodo={this.create} />
+                <ul>{todos}</ul>
             </div>
         );
     }
